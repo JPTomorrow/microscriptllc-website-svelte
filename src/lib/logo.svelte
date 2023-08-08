fb19e042dfae8f7a9016724d77a742159dbc34cf
JPTomorrow <justin.parker.morrow@gmail.com>
2023-08-08 07:59:25 -0600

    logo change

--- src/lib/logo.svelte
+++ src/lib/logo.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <a href="/" class="flex h-full items-start transition-transform hover:scale-105">
+	<p class="text-3xl font-thin">{env.PUBLIC_BRAND_NAME}</p>
 	<img
 		in:slide|global={{ delay: 0, axis: 'x', duration: 600 }}
 		alt="logo"
 		src="/images/logo.png"
-		class="hidden sm:block w-[40px] align-middle mr-5"
+		class="hidden sm:block w-[40px] align-middle ml-1"
 	/>
-	<p class="text-3xl font-thin">{env.PUBLIC_BRAND_NAME}</p>
 </a>
