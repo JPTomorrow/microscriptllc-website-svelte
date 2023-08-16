4ad4fade1d44393f568102fd13928ff53258eb16
JPTomorrow <justin.parker.morrow@gmail.com>
2023-08-16 14:06:46 -0600

    work on projects

--- src/lib/projectimage.svelte
+++ src/lib/projectimage.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
-	import Tiltcard from '$lib/tiltcard.svelte';
-	import Slideinellipticleft from '$lib/effects/slideinellipticleft.svelte';
-
 	export let src: string;
 </script>
 
-<Tiltcard size="w-full h-full">
-	<Slideinellipticleft>
-		<img
-			alt="projectimg"
-			{src}
-			class=" w-full h-full shadow-xl rounded-3xl border-[8px] border-white border-opacity-30"
-		/>
-	</Slideinellipticleft>
-</Tiltcard>
+<div class="h-[200px] flex">
+	<img alt="projectimg" {src} class="object-contain w-[100%] h-[100%] shadow-xl rounded-t-md" />
+</div>
