7e4ac5778a0598f222190a309fc65c80e497c62a
JPTomorrow <justin.parker.morrow@gmail.com>
2023-08-23 09:55:24 -0600

    finished contact page

---
+++ src/lib/effects/rollintext.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<div class="flex">
+	{#each text as c, i (i)}
+		<div class="roll-in-blurred-left w-full h-full" style={`animation-delay: ${i * 0.1}s`}>
+			{#if c != ' '}
+				<p class="text-xl">{c}</p>
+			{:else}
+				<p class="text-xl">&nbsp;</p>
+			{/if}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.roll-in-blurred-left {
+		-webkit-animation: roll-in-blurred-left 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
+		animation: roll-in-blurred-left 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
+	}
+	@-webkit-keyframes roll-in-blurred-left {
+		0% {
+			-webkit-transform: translateX(-1000px) rotate(-720deg);
+			transform: translateX(-1000px) rotate(-720deg);
+			-webkit-filter: blur(50px);
+			filter: blur(50px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: translateX(0) rotate(0deg);
+			transform: translateX(0) rotate(0deg);
+			-webkit-filter: blur(0);
+			filter: blur(0);
+			opacity: 1;
+		}
+	}
+	@keyframes roll-in-blurred-left {
+		0% {
+			-webkit-transform: translateX(-1000px) rotate(-720deg);
+			transform: translateX(-1000px) rotate(-720deg);
+			-webkit-filter: blur(50px);
+			filter: blur(50px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: translateX(0) rotate(0deg);
+			transform: translateX(0) rotate(0deg);
+			-webkit-filter: blur(0);
+			filter: blur(0);
+			opacity: 1;
+		}
+	}
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,44 +1,120 @@
-<script>
+<script lang="ts">
 	import { enhance } from '$app/forms';
+	import Rollintext from '$lib/effects/rollintext.svelte';
+	import { draw, fade } from 'svelte/transition';
 
-	function valEmail(e) {
-		console.log(e.target.value);
+	function valEmail(e: string) {
 		var emailRegEx =
 			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return emailRegEx.test(String(e.target.value).toLowerCase());
+		return emailRegEx.test(String(e).toLowerCase());
 	}
-	let isValid = false;
+	let email: string = '';
+	$: isValid = valEmail(email);
 
 	function sub() {
 		if (!isValid) {
 			return;
 		}
+		animateSend();
+	}
+
+	let isSending = false;
+	const pathColor = 'rgb(255, 235, 189)';
+	const fillColor = 'rgb(255, 235, 189, 0)';
+
+	function animateSend() {
+		isSending = true;
+		setTimeout(() => {
+			isSending = false;
+		}, 2000);
 	}
 </script>
 
-<div class="flex flex-col w-screen h-screen justify-center items-center">
-	<form
-		class="flex flex-col gap-5 items-start w-1/3"
-		method="POST"
-		on:submit|preventDefault={sub}
-		use:enhance
-		action="/contact/email"
-	>
-		<h1 class="text-2xl">Contact Us</h1>
-		<input class="input custom-input" type="text" placeholder="Name" name="name" />
-		{#if !isValid}
-			<h1 class="text-error-300">* Not a valid email.</h1>
-		{/if}
-		<input
-			on:input={(e) => valEmail(e)}
-			class="input custom-input"
-			type="text"
-			placeholder="Email"
-			name="email"
-		/>
-		<textarea class="textarea p-4 rounded-lg" rows="4" placeholder="Message" name="msg" />
-		<button class="btn variant-outline-primary rounded-full" type="submit">Submit</button>
-	</form>
+<div
+	in:fade={{ duration: 200 }}
+	class="flex flex-col w-screen h-screen justify-center items-center"
+>
+	{#if isSending}
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			version="1.1"
+			width="512"
+			height="512"
+			viewBox="0 0 256 256"
+			xml:space="preserve"
+		>
+			<defs />
+
+			<g transform="translate(32.807305370685654 32.807305370685654) scale(2.12 2.12)">
+				<path
+					d="M 82.127 71.258 H 26.728 c -4.341 0 -7.873 -3.532 -7.873 -7.873 v -10.16 c 0 -0.828 0.671 -1.5 1.5 -1.5 s 1.5 0.672 1.5 1.5 v 10.16 c 0 2.687 2.186 4.873 4.873 4.873 h 55.399 c 2.687 0 4.873 -2.187 4.873 -4.873 v -36.77 c 0 -2.687 -2.187 -4.873 -4.873 -4.873 H 26.728 c -2.687 0 -4.873 2.186 -4.873 4.873 v 10.038 c 0 0.829 -0.671 1.5 -1.5 1.5 s -1.5 -0.671 -1.5 -1.5 V 26.615 c 0 -4.341 3.532 -7.873 7.873 -7.873 h 55.399 c 4.341 0 7.873 3.532 7.873 7.873 v 36.77 C 90 67.726 86.468 71.258 82.127 71.258 z"
+					stroke-linecap="round"
+					fill={fillColor}
+					stroke={pathColor}
+					stroke-width={0.5}
+					in:draw={{ duration: 1400 }}
+				/>
+				<path
+					d="M 63.732 45 l 16.726 -15.344 c 0.61 -0.56 0.651 -1.509 0.092 -2.12 c -0.56 -0.61 -1.508 -0.65 -2.119 -0.091 L 54.427 49.466 L 30.425 27.445 c -0.611 -0.559 -1.559 -0.519 -2.12 0.091 c -0.56 0.61 -0.519 1.56 0.091 2.12 L 45.122 45 L 28.396 60.345 c -0.61 0.561 -0.651 1.509 -0.091 2.119 c 0.296 0.323 0.7 0.486 1.106 0.486 c 0.362 0 0.726 -0.131 1.014 -0.395 l 16.916 -15.52 l 6.072 5.571 c 0.286 0.263 0.65 0.395 1.014 0.395 s 0.728 -0.132 1.014 -0.395 l 6.073 -5.571 l 16.917 15.52 c 0.288 0.264 0.651 0.395 1.014 0.395 c 0.405 0 0.81 -0.163 1.105 -0.486 c 0.56 -0.61 0.519 -1.559 -0.092 -2.119 L 63.732 45 z"
+					stroke-linecap="round"
+					fill={fillColor}
+					stroke={pathColor}
+					stroke-width={0.5}
+					in:draw={{ duration: 1400 }}
+				/>
+				<path
+					d="M 25.423 46.5 H 7.539 c -0.829 0 -1.5 -0.671 -1.5 -1.5 s 0.671 -1.5 1.5 -1.5 h 17.885 c 0.829 0 1.5 0.671 1.5 1.5 S 26.252 46.5 25.423 46.5 z"
+					stroke-linecap="round"
+					fill={fillColor}
+					stroke={pathColor}
+					stroke-width={0.5}
+					in:draw={{ duration: 1400 }}
+				/>
+				<path
+					d="M 12.497 36.016 H 1.5 c -0.829 0 -1.5 -0.671 -1.5 -1.5 s 0.671 -1.5 1.5 -1.5 h 10.997 c 0.829 0 1.5 0.671 1.5 1.5 S 13.326 36.016 12.497 36.016 z"
+					stroke-linecap="round"
+					fill={fillColor}
+					stroke={pathColor}
+					stroke-width={0.5}
+					in:draw={{ duration: 1400 }}
+				/>
+				<path
+					d="M 12.497 56.984 H 4.394 c -0.829 0 -1.5 -0.672 -1.5 -1.5 s 0.671 -1.5 1.5 -1.5 h 8.104 c 0.829 0 1.5 0.672 1.5 1.5 S 13.326 56.984 12.497 56.984 z"
+					stroke-linecap="round"
+					fill={fillColor}
+					stroke={pathColor}
+					stroke-width={0.5}
+					in:draw={{ duration: 1400 }}
+				/>
+			</g>
+		</svg>
+	{:else}
+		<form
+			class="flex flex-col gap-5 items-start w-1/3"
+			method="POST"
+			on:submit|preventDefault={sub}
+			use:enhance
+			action="/contact/email"
+			in:fade={{ duration: 800 }}
+		>
+			<!-- <Rollintext text="Contact Us" /> -->
+			<h1 class="text-2xl">Contact Us</h1>
+			<input class="input custom-input" type="text" placeholder="Name" name="name" />
+			{#if !isValid}
+				<h1 class="text-error-500">* Not a valid email</h1>
+			{/if}
+			<input
+				bind:value={email}
+				class="input custom-input"
+				type="text"
+				placeholder="Email"
+				name="email"
+			/>
+			<textarea class="textarea p-4 rounded-lg" rows="4" placeholder="Message" name="msg" />
+			<button class="btn variant-outline-secondary rounded-full" type="submit">Submit</button>
+		</form>
+	{/if}
 </div>
 
 <style lang="postcss">
