97ef00e32854c1711e0e6df4918b20e72eaaa79b
JPTomorrow <justin.parker.morrow@gmail.com>
2023-09-10 12:24:38 -0600

    fixed buttons shifting layout

---
+++ src/lib/banner-image.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+</script>
+
+<div in:slide|global class="relative aspect-[1.49]">
+	<img src="/images/about-pic.webp" width="100%" class="utah-img-clip" alt="Utah" />
+</div>
+
+<style lang="postcss">
+	.utah-img-clip {
+		clip-path: polygon(2% 0%, 100% 0%, 100% 100%, 2% 100%, 10% 50%);
+		box-sizing: border-box;
+		@apply border-[1px] border-r-[10px] border-secondary-600 rounded-t-md rounded-b-md;
+	}
+</style>
