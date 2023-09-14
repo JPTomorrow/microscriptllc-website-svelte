cf86168b49f854d42608864f219029d93d457f55
JPTomorrow <justin.parker.morrow@gmail.com>
2023-09-14 14:47:55 -0600

    SEO optimize round #4

--- src/lib/banner-image.svelte
+++ src/lib/banner-image.svelte
@@ -1,11 +1,17 @@
-<div class="relative w-full min-h-full aspect-[1.49]">
-	<img src="/images/about-pic.webp" width="100%" class="utah-img-clip" alt="Utah" />
+<div class="relative w-full min-h-full">
+	<img
+		src="/images/about-pic.webp"
+		width="1440px"
+		height="969px"
+		class="utah-img-clip"
+		alt="Utah"
+	/>
 </div>
 
 <style lang="postcss">
 	.utah-img-clip {
 		clip-path: polygon(2% 0%, 100% 0%, 100% 100%, 2% 100%, 10% 50%);
 		box-sizing: border-box;
-		@apply border-[1px] border-r-[10px] border-secondary-600 rounded-t-md rounded-b-md;
+		@apply w-full h-auto border-[1px] border-r-[10px] border-secondary-600 rounded-t-md rounded-b-md;
 	}
 </style>
