5dbb4b6906ccc8ba78b00ab7fdcfbf3dfdfd0685
JPTomorrow <justin.parker.morrow@gmail.com>
2023-09-15 05:40:03 -0600

    fixing banner hieght on chrome

--- src/lib/banner-image.svelte
+++ src/lib/banner-image.svelte
@@ -1,5 +1,5 @@
 <!-- <div class="relative w-full min-h-full"> -->
-<img src="/images/about-pic.webp" width="1440px" height="969px" class="utah-img-clip" alt="Utah" />
+<img src="/images/about-pic.webp" width="500px" class="utah-img-clip" alt="Utah" />
 
 <!-- </div> -->
 
@@ -7,10 +7,9 @@
 	.utah-img-clip {
 		clip-path: polygon(2% 0%, 100% 0%, 100% 100%, 2% 100%, 10% 50%);
 		box-sizing: border-box;
-		width: 100%;
 		height: auto;
 		background-size: cover;
-		aspect-ratio: 1440/960;
+		aspect-ratio: 1440/969;
 		@apply hidden lg:inline-block h-full border-[1px] border-r-[10px] border-secondary-600 rounded-t-md rounded-b-md;
 	}
 </style>
