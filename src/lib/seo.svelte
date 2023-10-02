f9e772e7be1041e102821bb9b13beed39af5b701
JPTomorrow <justin.parker.morrow@gmail.com>
2023-10-01 22:39:28 -0600

    seo #4

--- src/lib/seo.svelte
+++ src/lib/seo.svelte
@@ -12,12 +12,12 @@
 	<title>{title} | {PUBLIC_BRAND_NAME}</title>
 	<meta
 		name="keywords"
-		content="cannabis, cannnabis dispensary, buy cannabis online, weed, smoke, shop online, ecommerce, ai, ai bud tender, ai budtender, cannabis consulting"
+		content="Software Engineer, Software Developer, Web Development, Web Developer, Web Designer, Web Engineer, OpenAI, ai, ai consulting, photography, image generation, SEO"
 	/>
 	<meta
 		name="description"
 		content="{description +
-			'. '}Discover a unique cannabis shopping experience at {PUBLIC_BRAND_NAME}, your friendly neighborhood dispensary. Offering both human and AI budtender consultations to cater to your preferences, along with a seamless online shopping platform. Explore our diverse range of quality cannabis products and get personalized recommendations effortlessly. Your journey towards finding the perfect blend starts here at {PUBLIC_BRAND_NAME}."
+			'. '}Unlock your business potential with MicroScript LLC's top-notch web design services. Transform your online presence today. Learn more now!"
 	/>
 	{@html `<script type="application/ld+json">
                 {
