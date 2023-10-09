61eb7179f46922afe20d3cf37a5cee2a2b0c1850
JPTomorrow <justin.parker.morrow@gmail.com>
2023-10-09 10:15:53 -0600

    migrated database to its own

--- drizzle/migrations/0000_nosy_screwball.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `blog-posts` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`headline` text NOT NULL,
-	`body` text NOT NULL,
-	`created_at` integer DEFAULT (cast (unixepoch() as int)) NOT NULL
-);
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,15 +1,12 @@
-import { relations, sql } from 'drizzle-orm';
-import { index, integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const blogPosts = sqliteTable(
-  'blog-posts',
-  {
-    id: text('id').primaryKey(),
-    name: text('name').notNull(),
-    headline: text('headline').notNull(),
-    body: text('body').notNull(),
-    createdAt: integer('created_at')
-      .notNull()
-      .default(sql`(cast (unixepoch() as int))`)
-  }
-);
+export const blogPosts = sqliteTable('blog-posts', {
+	id: text('id').primaryKey(),
+	name: text('name').notNull(),
+	headline: text('headline').notNull(),
+	body: text('body').notNull(),
+	createdAt: integer('created_at')
+		.notNull()
+		.default(sql`(cast (unixepoch() as int))`)
+});
