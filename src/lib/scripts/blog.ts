6c59567e34245ff1a8cb8c095a71528740432cd4
JPTomorrow <justin.parker.morrow@gmail.com>
2023-10-25 08:03:03 -0600

    fixed blog post URL's

--- src/lib/scripts/blog.ts
+++ src/lib/scripts/blog.ts
@@ -2,9 +2,11 @@
 export const formBlogPostSlug = (post_name: string, post_id: Number = -1): string => {
 	// strip out all non-alphanumeric characters
 	const nameStripped = post_name
+		.replaceAll('<h1>', '')
+		.replaceAll('</h1>', '')
 		.replace(/[^a-zA-Z0-9\s]/g, '')
 		.split(' ')
-		.filter((s, i) => i <= 3)
+		.filter((_, i) => i <= 3)
 		.join(' ')
 		.replaceAll(' ', '-')
 		.toLowerCase();
