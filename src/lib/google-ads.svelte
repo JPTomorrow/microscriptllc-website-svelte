02aed8a7d24b4cd8e1536ff05ce6596c233da04a
JPTomorrow <justin.parker.morrow@gmail.com>
2023-11-22 02:37:17 -0700

    crossorigin

--- src/lib/google-ads.svelte
+++ src/lib/google-ads.svelte
@@ -1,6 +1,10 @@
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YWDL76T2MS"></script>
-	<script>
+	<script
+		crossorigin="anonymous"
+		async
+		src="https://www.googletagmanager.com/gtag/js?id=G-YWDL76T2MS"
+	></script>
+	<script crossorigin="anonymous">
 		window.dataLayer = window.dataLayer || [];
 		window.gtag = function gtag() {
 			dataLayer.push(arguments);
@@ -10,7 +14,7 @@
 		window.gtag('config', 'G-YWDL76T2MS');
 	</script>
 
-	<script>
+	<script crossorigin="anonymous">
 		window.gtag_report_conversion = function gtag_report_conversion(url) {
 			var callback = function () {
 				console.log('gtag conversion');
