f3453178a049116ff4eadd39ba8c831e4ddcea2e
JPTomorrow <justin.parker.morrow@gmail.com>
2023-11-22 00:04:49 -0700

    fixing google ads args

--- src/lib/google-ads.svelte
+++ src/lib/google-ads.svelte
@@ -1,16 +1,12 @@
-<script lang="ts">
-	if (typeof window !== 'undefined') {
-		window.dataLayer = window.dataLayer || [];
-		window.gtag = function gtag(): void {
-			window.dataLayer.push(arguments);
-		};
-		window.gtag('js', new Date());
-
-		window.gtag('config', 'G-YWDL76T2MS');
-	}
-</script>
-
 <svelte:head>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YWDL76T2MS">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag(...args) {
+			window.dataLayer.push(args);
+		};
+		window.gtag('js', new Date());
+		window.gtag('config', 'G-YWDL76T2MS');
 	</script>
 </svelte:head>
