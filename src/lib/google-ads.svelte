40c31a7a53f460076c4840e86c97b1703721f86d
JPTomorrow <justin.parker.morrow@gmail.com>
2023-11-22 02:20:37 -0700

    getting tired of gtag

--- src/lib/google-ads.svelte
+++ src/lib/google-ads.svelte
@@ -1,14 +1,13 @@
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YWDL76T2MS">
-	</script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YWDL76T2MS"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-		window.gtag = function gtag(...args) {
-			window.dataLayer.push(args);
-			console.log(`gtag: ${window.dataLayer}`);
+		window.gtag = function gtag() {
+			dataLayer.push(arguments);
 		};
-		window.gtag('js', new Date());
-		window.gtag('config', 'AW-11414972997');
+		gtag('js', new Date());
+
+		gtag('config', 'G-YWDL76T2MS');
 	</script>
 
 	<script>
